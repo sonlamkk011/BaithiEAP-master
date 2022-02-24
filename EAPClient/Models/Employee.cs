34a8554d43b3c2327cc4561e4b33143e65525bf2
sonla <sonla@DESKTOP-MCKQQ51>
2022-02-24 10:28:59 +0700

    Add project files.

---
+++ EAPClient/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EAPClient.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
